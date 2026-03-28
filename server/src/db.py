261db9876cf42dc4ba01ebfca74b098ec17bff80
Guilherme Pedroza <guilhermebarb0sa@proton.me>
2026-03-28 01:33:58 -0300

    fix: extra=ignore

--- server/src/db.py
+++ server/src/db.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     database_url: str
 
-    model_config = SettingsConfigDict(env_file="../.env.local", extra=ignore)
+    model_config = SettingsConfigDict(env_file="../.env.local", extra="ignore")
 
 settings = Settings()
 
