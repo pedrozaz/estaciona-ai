6269da42a395c326afc5fe6c92b280f0b8154d36
Guilherme Pedroza <guilhermebarb0sa@proton.me>
2026-03-29 13:47:27 -0300

    feat(server): run cargo fmt

--- server_rust/src/state.rs
+++ server_rust/src/state.rs
@@ -14,7 +14,7 @@ pub type SharedState = Arc<RwLock<HashMap<String, SpotStatus>>>;
 
 pub fn init_state() -> SharedState {
     let mut map = HashMap::new();
-    
+
     map.insert("A-01".to_string(), SpotStatus::Free);
     map.insert("A-02".to_string(), SpotStatus::Free);
     Arc::new(RwLock::new(map))
