10d11a3366ed8159ab784df0bb06aac88a327043
Guilherme Pedroza <guilhermebarb0sa@proton.me>
2026-06-09 20:57:37 -0300

    feat(core): add pub/sub event bus for decoupled modular communication

---
+++ web/js/admin/bus.js
@@ -0,0 +1,38 @@
+class EventBus {
+    constructor() {
+        this.listeners = {};
+    }
+
+    on(event, callback) {
+        if (!this.listeners[event]) {
+            this.listeners[event] = [];
+        }
+        this.listeners[event].push(callback);
+        return () => this.off(event, callback);
+    }
+
+    off(event, callback) {
+        if (!this.listeners[event]) return;
+        this.listeners[event] = this.listeners[event].filter(cb => cb !== callback);
+    }
+
+    emit(event, data = null) {
+        if (!this.listeners[event]) return;
+        this.listeners[event].forEach(callback => {
+            try {
+                callback(data);
+            } catch (error) {
+                console.error(error);
+            }
+        });
+    }
+
+    once(event, callback) {
+        const unsubscribe = this.on(event, (data) => {
+            unsubscribe();
+            callback(data);
+        });
+    }
+}
+
+export const bus = new EventBus();
